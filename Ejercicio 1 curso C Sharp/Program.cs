43fabedf70902543a825f18aee4ac965c6c52bcb
Sergi Alesanco Perich <sergi.alesanco.perich@gmail.com>
2022-10-20 11:07:13 +0200

    Add project files.

---
+++ Ejercicio 1 curso C Sharp/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+string nombre;
+
+Console.WriteLine("Intorduce tu nombre: ");
+nombre = Console.ReadLine();
+
+Console.WriteLine("Tu nombre es " + nombre);
---
+++ Ejercicio 2 curso C Sharp/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+string hora;
+
+Console.WriteLine("Intorduce la hora: ");
+hora = Console.ReadLine();
+
+Console.WriteLine("Son las " + hora);
